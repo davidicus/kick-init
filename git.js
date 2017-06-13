34fa3fd61725cde2c9c069f9610dd479801b4331
David Conner <david@david-conner.com>
2017-06-13 13:14:39 -0500

    remove readline package

--- git.js
+++ git.js
@@ -3,7 +3,6 @@
 const fs = require('fs');
 const path = require('path');
 const { spawn } = require('child_process');
-const readline = require('readline');
 
 //path to gitbash script
 const bin = (__filename).replace(/git.js/i, 'gitbash.sh');
